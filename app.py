18f16c9740ae8d6bbde598667c67b422bc98ddb8
Tawe Nganga <tawenga@yandex.com>
2018-08-12 11:52:43 +0300

    Initial deploy init

---
+++ app.py
@@ -0,0 +1,186 @@
+from flask import Flask, g, request, abort
+from flask import jsonify
+from flask_script import Manager
+from flask_script import Shell
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_httpauth import HTTPBasicAuth
+from passlib.apps import custom_app_context as pwd_context
+from flask_msearch import Search
+import os
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kelis:password@localhost/kelis'
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://kelis:password@localhost/kelis')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MSEARCH_INDEX_NAME'] = 'whoosh_index'
+app.config['MSEARCH_BACKEND'] = 'whoosh'
+app.config['MSEARCH_ENABLE'] = True
+manager = Manager(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+auth = HTTPBasicAuth()
+search = Search()
+search.init_app(app)
+
+
+@app.route('/api/users', methods = ['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400) # missing arguments
+    if User.query.filter_by(username = username).first() is not None:
+        abort(400) # existing user
+    user = User(username = username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({ 'id': user.id })
+
+@auth.verify_password
+def verify_password(username, password):
+    user = User.query.filter_by(username = username).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.user = user
+    return True
+
+@auth.login_required
+@app.route('/api/login', methods=['POST'])
+def login():
+    username = request.json.get('username')
+    user = User.query.filter_by(username = username).first()
+    if user is not None:
+        return jsonify({ 'id': user.id, 'status': True })
+    return jsonify({ 'status': True })
+
+@app.route('/api/users/<int:id>')
+def get_user(id):
+    user = User.query.get_or_404(id)
+    return jsonify(user.to_json())
+
+#get all profiles
+@app.route('/api/profiles', methods=['GET'])
+def get_user_profiles():
+    user_profiles = UserProfile.query.order_by(UserProfile.thumbs_up.desc()).all()
+    return jsonify({
+        'user_profiles': [user_profile.to_json() for user_profile in user_profiles],
+    })
+
+#create new profile
+@app.route('/api/profiles', methods=['POST'])
+def new_profile():
+    profile = UserProfile.from_json(request.json)
+    db.session.add(profile)
+    db.session.commit()
+    return jsonify(profile.to_json())
+
+#get single profile
+@app.route('/api/profiles/<int:id>')
+def get_profiles(id):
+    user_profile = UserProfile.query.get_or_404(id)
+    return jsonify(user_profile.to_json())
+
+#edit profile
+@app.route('/api/profiles/<int:id>', methods=['PUT'])
+def update_profile(id):
+    user_profile = UserProfile.query.get(id)
+
+    user_id = request.json['user_id']
+    username = request.json['username']
+    course_name_and_year = request.json['course_name_and_year']
+    photo = request.json['photo']
+    thumbs_up = request.json['thumbs_up']
+    thumbs_down = request.json['thumbs_down']
+
+    user_profile.user_id = user_id
+    user_profile.username = username
+    user_profile.course_name_and_year = course_name_and_year
+    user_profile.photo = photo
+    user_profile.thumbs_up = thumbs_up
+    user_profile.thumbs_down = thumbs_down
+
+    db.session.commit()
+    return jsonify(user_profile.to_json())
+
+# views.py
+@app.route("/api/search",  methods=['POST'])
+def w_search():
+    keyword = request.json.get('keyword')
+    user_profiles = UserProfile.query.msearch(keyword).all()
+    #user_profiles = UserProfile.query.msearch(keyword).order_by(UserProfile.thumbs_up.desc()).all()   ##switch to this
+    return jsonify({
+        'user_profiles': [user_profile.to_json() for user_profile in user_profiles],
+    })
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class UserProfile(db.Model):
+    __tablename__ = 'user_profiles'
+    __searchable__ = ['username', 'course_name_and_year']
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    username = db.Column(db.String(64), unique=True, index=True)
+    course_name_and_year = db.Column(db.String(64), index=True)
+    photo = db.Column(db.String(64))
+    thumbs_up = db.Column(db.Integer)
+    thumbs_down = db.Column(db.Integer)
+
+    def to_json(self):
+        json_user_profile = {
+            'id': self.id,
+            'user_id': self.user_id,
+            'username': self.username,
+            'course_name_and_year': self.course_name_and_year,
+            'photo': self.photo,
+            'thumbs_up': self.thumbs_up,
+            'thumbs_down': self.thumbs_down
+        }
+        return json_user_profile
+
+    @staticmethod
+    def from_json(user_profile):
+        user_id = user_profile.get('user_id')
+        username = user_profile.get('username')
+        course_name_and_year = user_profile.get('course_name_and_year')
+        photo = user_profile.get('photo')
+        thumbs_up = user_profile.get('thumbs_up')
+        thumbs_down = user_profile.get('thumbs_down')
+        return UserProfile(user_id = user_id,
+                           username = username,
+                           course_name_and_year = course_name_and_year,
+                           photo = photo,
+                           thumbs_up = thumbs_up,
+                           thumbs_down = thumbs_down)
+
+    def __repr__(self):
+        return '<UserProfile %r>' % self.username
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, UserProfile=UserProfile)
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    search.create_index(update=True)
+    manager.run()
---
+++ venv/lib/python3.6/site-packages/flask_msearch/backends.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# **************************************************************************
+# Copyright © 2017 jianglin
+# File Name: backends.py
+# Author: jianglin
+# Email: xiyang0807@gmail.com
+# Created: 2017-04-15 20:03:27 (CST)
+# Last Update: Friday 2018-06-15 10:26:36 (CST)
+#          By:
+# Description:
+# **************************************************************************
+import logging
+import sys
+
+log_console = logging.StreamHandler(sys.stderr)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(log_console)
+
+
+def relation_column(instance, fields):
+    '''
+    such as: user.username
+    such as: replies.content
+    '''
+    relation = getattr(instance.__class__, fields[0]).property
+    _field = getattr(instance, fields[0])
+    if relation.lazy == 'dynamic':
+        _field = _field.first()
+    return getattr(_field, fields[1]) if _field else ''
+
+
+class BaseBackend(object):
+    def __init__(self, app=None, db=None, analyzer=None):
+        """
+        You can custom analyzer by::
+
+            from jieba.analyse import ChineseAnalyzer
+            search = Search(analyzer = ChineseAnalyzer)
+        """
+        self.db = db
+        self.analyzer = analyzer
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        self.app = app
+        if not self.db:
+            self.db = self.app.extensions['sqlalchemy'].db
+        self.db.Model.query_class = self._query_class(
+            self.db.Model.query_class)
+
+    def _query_class(self, q):
+        _self = self
+
+        class Query(q):
+            def msearch(self, query, fields=None, limit=None, or_=False):
+                model = self._mapper_zero().class_
+                return _self.msearch(model, query, fields, limit, or_)
+
+        return Query
+
+    def create_index(self,
+                     model='__all__',
+                     update=False,
+                     delete=False,
+                     yield_per=100):
+        if model == '__all__':
+            return self.create_all_index(update, delete)
+        ix = self._index(model)
+        instances = model.query.enable_eagerloads(False).yield_per(yield_per)
+        for instance in instances:
+            self.create_one_index(instance, update, delete, False)
+        ix.commit()
+        return ix
+
+    def create_all_index(self, update=False, delete=False, yield_per=100):
+        all_models = self.db.Model._decl_class_registry.values()
+        models = [i for i in all_models if hasattr(i, '__searchable__')]
+        ixs = []
+        for m in models:
+            ix = self.create_index(m, update, delete, yield_per)
+            ixs.append(ix)
+        return ixs
+
+    def update_one_index(self, instance, commit=True):
+        return self.create_one_index(instance, update=True, commit=commit)
+
+    def delete_one_index(self, instance, commit=True):
+        return self.delete_one_index(instance, delete=True, commit=commit)
+
+    def update_all_index(self, yield_per=100):
+        return self.create_all_index(update=True, yield_per=yield_per)
+
+    def delete_all_index(self, yield_per=100):
+        return self.create_all_index(delete=True, yield_per=yield_per)
+
+    def update_index(self, model='__all__', yield_per=100):
+        return self.create_index(model, update=True, yield_per=yield_per)
+
+    def delete_index(self, model='__all__', yield_per=100):
+        return self.create_index(model, delete=True, yield_per=yield_per)
+
+    def _index_signal(self, sender, changes):
+        for change in changes:
+            instance = change[0]
+            operation = change[1]
+            if hasattr(instance, '__searchable__'):
+                if operation == 'insert':
+                    self.create_one_index(instance)
+                elif operation == 'update':
+                    self.create_one_index(instance, update=True)
+                elif operation == 'delete':
+                    self.create_one_index(instance, delete=True)
+
+            prepare = [i for i in dir(instance) if i.startswith('msearch_')]
+            for p in prepare:
+                if operation == 'delete':
+                    attrs = getattr(instance, p)(delete=True)
+                else:
+                    attrs = getattr(instance, p)()
+                ix = self._index(attrs.pop('_index'))
+                if attrs['attrs']:
+                    for attr in attrs['attrs']:
+                        ix.update(**self._fields(attr))
+                    ix.commit()
+
+    def whoosh_search(self, m, query, fields=None, limit=None, or_=False):
+        logger.warning(
+            'whoosh_search has been replaced by msearch.please use msearch')
+        return self.msearch(m, query, fields, limit, or_)
+
+    # def msearch(self, m, query, fields=None, limit=None, or_=False):
+    #     raise NotImplementedError
